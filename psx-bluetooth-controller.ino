00a7f0653d05d3d930bcb2682bcfdd600a598165
Cosmin Stamate <cos.stamate@gmail.com>
2019-03-15 00:18:33 +0200

    Implement gamepad -> keyboard mapping

--- psx-bluetooth-controller.ino
+++ psx-bluetooth-controller.ino
@@ -27,6 +27,7 @@
 #include "Adafruit_BluefruitLE_UART.h"
 #include "BluefruitConfig.h"
 #include "PS2X_lib.h"
+#include "HID_Keycodes.h"
 /*********************************************************************************/
 
 
@@ -36,9 +37,9 @@
 #define PSX_DAT             5
 #define PSX_CMD             6
 #define PSX_ATT             9
-#define PSX_CLK            10
+#define PSX_CLK             10
 #define ANALOG_DEADZONE     25
-#define GAMEPAD_READ_DELAY  500
+#define GAMEPAD_READ_DELAY  50
 #define SERIAL_RATE         115200
 /*********************************************************************************/
 
@@ -52,10 +53,36 @@ PS2X gamepad;
 
 // GLOBALS & HELPERS
 /*********************************************************************************/
+int buttons[14] = {
+  PSB_TRIANGLE, PSB_CIRCLE, PSB_CROSS, PSB_SQUARE,
+  PSB_PAD_UP, PSB_PAD_RIGHT, PSB_PAD_DOWN, PSB_PAD_LEFT,
+  PSB_L1, PSB_L2,
+  PSB_R1, PSB_R2,
+  PSB_SELECT, PSB_START
+};
+
+int buttonKeyCodes[14] = {
+  HID_KEY_Q, HID_KEY_R, HID_KEY_S, HID_KEY_T,
+  HID_KEY_ARROW_UP, HID_KEY_ARROW_RIGHT, HID_KEY_ARROW_DOWN, HID_KEY_ARROW_LEFT,
+  HID_KEY_A, HID_KEY_S,
+  HID_KEY_D, HID_KEY_F,
+  HID_KEY_TAB, HID_KEY_RETURN
+};
+
+int pressedKeyCodes[14] = { 0 };
+
+unsigned int buttonState = 0;
+
 // Error helper function.
 void error(const __FlashStringHelper*err) {
   Serial.println(err);
   while (1);
+}
+
+String printHex(int input) {
+  char s[2];
+  sprintf(s, "%02x", input);
+  return String(s);
 }
 /*********************************************************************************/
 
@@ -70,13 +97,13 @@ void setup(void) {
   Serial.println(F("------------------------------------------------"));
 
   Serial.print(F("--> Initializing the Bluefruit LE module: "));
-  if (!ble.begin(VERBOSE_MODE)) { error(F("ERROR: Couldn not find the module.")); }
+  if (!ble.begin(VERBOSE_MODE)) { error(F("ERROR: Could not find the module.")); }
   Serial.println(F("OK!"));
 
   /* Perform a factory reset to make sure everything is in a known state. */
   if (FACTORYRESET_ENABLE) {
     Serial.print(F("--> Performing a factory reset: "));
-    if (!ble.factoryReset()){ error(F("ERROR: Couldn not factory reset.")); }
+    if (!ble.factoryReset()){ error(F("ERROR: Could not factory reset.")); }
     Serial.println(F("OK!"));
   }
 
@@ -94,7 +121,7 @@ void setup(void) {
 
   /* Enable HID Service. */
   Serial.print(F("--> Enabling HID service: "));
-  if (!ble.sendCommandCheckOK(F("AT+BLEHIDEN=1"))) { error(F("ERROR: Could not enable HID service.")); }
+  if (!ble.sendCommandCheckOK(F("AT+BLEHIDEN=1"))) { error(F("ERROR: Could not enable HID keyboard service.")); }
   Serial.println(F("OK!"));
 
   /* Adding or removing requires a reset. */
@@ -117,5 +144,33 @@ void setup(void) {
 /*********************************************************************************/
 void loop() {
   gamepad.read_gamepad(false, 0);
+
+  unsigned int currentButtonState = gamepad.ButtonDataByte();
+  if (buttonState == currentButtonState) { return; }
+  buttonState = currentButtonState;
+
+  memset(pressedKeyCodes, 0, sizeof(pressedKeyCodes));
+  int pressedIndex = 0;
+  int i;
+  
+  for (i = 0; i < 14; i++) {
+    if (!gamepad.Button(buttons[i])) { continue; }
+    pressedKeyCodes[pressedIndex] = buttonKeyCodes[i];
+    pressedIndex++;
+  }
+
+  String command = String("AT+BLEKEYBOARDCODE=00-00");
+  for (int i = 0; i < 5; i++) {
+    command += String("-") + printHex(pressedKeyCodes[i]);
+  }
+  
+  Serial.print("Sending key code '");
+  Serial.print(command);
+  Serial.println("': ");
+  
+  ble.println(command);
+  if (ble.waitForOK()) { Serial.println(F("OK!")); }
+  else { Serial.println(F("FAILED!")); }
+  
   delay(GAMEPAD_READ_DELAY);
 }
